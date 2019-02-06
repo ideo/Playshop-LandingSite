0b8a88292f112fd04224adaae2a5839958a4e725
Takashi Wickes <twickes@ideo.com>
2019-02-06 15:22:28 -0800

    Initial development of site

---
+++ playshop/src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import logo from './assets/D4Play_logo.jpg';
+import inspirationTourBalloon from './assets/InspirationTourBalloon.png';
+import moon from './assets/moon.png';
+import { ReactComponent as SummaryLine } from './assets/SummaryLine.svg';
+import { ReactComponent as InspirationLine } from './assets/InspirationLine.svg';
+import { ReactComponent as WorkshopLine } from './assets/WorkshopLine.svg';
+import { ReactComponent as PopUpLine } from './assets/PopUpLine.svg';
+import './App.css';
+import WorkshopCard from './WorkshopCard.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.summaryRowRef = React.createRef();
+    // this.state.showSummaryRow = "summaryRow";
+    this.state = {showSummaryRow: "summaryRow", summaryRowHeight: "100%"}
+    // this.workshops 
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+  render() {
+    return (
+      <div className="App" onScroll={this.handleScroll}>
+        <header className="App-header">
+          <div className="logoContainer">
+            <img src={logo} />
+          </div>
+          <div className="menuContainer">
+            <p>OUR APPS / SESAME STREET / FISHER-PRICE / SUPPORT</p>
+          </div>
+        </header>
+        <div className="App-container"> 
+          <div className="heroRow">
+            <div className="videoContainer">
+              <div className="video"></div>
+            </div>
+            <div className="heroTextContainer">
+              <div className="heroText">
+                <h1>Make with Us in Playshops</h1>
+                <p>Generate new concepts, build innovative culture, and learn new design tools with IDEO's Play Lab</p>
+                <button>Join Us</button>
+              </div>
+            </div>
+            <div className="summaryRow" ref={this.summaryRowRef}>
+              <div className="summaryBackground" style={{height: this.state.summaryRowHeight}}>
+                <SummaryLine />
+                <SummaryLine className="backPath"/>
+              </div>
+              <div className="summary">
+                  <h2>here's the deal</h2>
+                  <h1>IDEO's Play Lab offers a design-thinking workshop with a playful twist!</h1>
+                  <p>Your team will walk away with new tools, new ideas, and new methodologies for innovation. Perfect for tackling old challenges, building positive company culture, and breaking down organizational barriers.</p>
+              </div>
+            </div>
+            <div className="workshopCardsContainer">
+              <p>We offer 3 types of <br/>Play Lab Workshops:</p>
+
+              <WorkshopCard 
+                  title="inspirationTour" 
+                  image = {inspirationTourBalloon}
+                  backgroundLine = {<InspirationLine />}
+                  textSide="left" 
+                  color="#67DDAB"
+                  durationText="1 - 8 Hours"
+                  headerText={<h1>Need a <span>boost</span> of energy, momentum, creativity, or innovation? <br />Let's go on <span>an <br/>Inspiration Tour.</span></h1>}
+                  idealForText="Teams looking to broaden their perspective, gain insights from innovation in adjacent industries, and rinse out stress with laughter and delight."
+              />
+              <WorkshopCard 
+                  title="playWorkshop" 
+                  image = {moon}
+                  backgroundLine = {<WorkshopLine />}
+                  textSide="right" 
+                  color="#FF9C1C"
+                  durationText="2 - 8 Hours"
+                  headerText={<h1>Our <span>Play Workshops</span> are interactive, action-oriented, and filled with laughter.</h1>}
+                  idealForText="Teams looking for a memorable high-energy experience that can take place in one room as a stand alone event or as a portion of a larger program."
+              />
+              <WorkshopCard 
+                  title="popUp" 
+                  image = {inspirationTourBalloon}
+                  backgroundLine = {<PopUpLine />}
+                  textSide="left" 
+                  color="#8ECCEC"
+                  durationText="1 Day - 2 Weeks"
+                  headerText={<h1>Our <span>Play Workshops</span> are interactive, action-oriented, and filled with laughter.</h1>}
+                  idealForText="Teams that want to move quickly from ideas to tangilble prototypes by leverageing the science of play."
+              />
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+  handleScroll(event) {
+    var heightBound = window.height * 0.8
+    // this.state.showSummaryRow = "active";
+    console.log(this.summaryRowRef.current.clientHeight)
+    var summaryRowHeight = this.summaryRowRef.current.clientHeight + 60 + 40;
+    this.setState({showSummaryRow: "summaryRow active", summaryRowHeight: summaryRowHeight + "px"})
+    // console.log(event)
+    // console.log(this.summaryRef.current)
+    // const node = this.summaryRef.current;
+    // node.style.animation = {animation: "fillLeft 1s ease"}
+    // this.summaryRef.setState({animation: 'fillLeft 1s ease'});
+    
+  }
+}
+
+export default App;
---
+++ playshop/src/WorkshopCard.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { ReactComponent as SummaryLine } from './assets/SummaryLine.svg';
+import { ReactComponent as InspirationLine } from './assets/InspirationLine.svg';
+import { ReactComponent as WorkshopLine } from './assets/WorkshopLine.svg';
+import './App.css';
+
+class WorkshopCard extends Component {
+    render() {
+        return(
+            <div className={"workshopCard " + this.props.title} >
+                    <div className="workshopBackground">
+                        {this.props.backgroundLine}
+                    </div>
+                    <div className="workshopCardText" style={{float: this.props.textSide}}>
+                        <h2 style={{color: this.props.color}}>( {this.props.durationText} )</h2>
+                        {this.props.headerText}
+                        <div className="idealForCard " style={{backgroundColor: this.props.color}}>
+                            <h3>IDEAL FOR:</h3>
+                            <p>{this.props.idealForText}</p>
+                        </div>
+                        <button style={{color: "black", borderColor: this.props.color}}>Learn More</button>
+                    </div>
+                <div className="workshopCardImage">
+                <img src={this.props.image} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default WorkshopCard;
