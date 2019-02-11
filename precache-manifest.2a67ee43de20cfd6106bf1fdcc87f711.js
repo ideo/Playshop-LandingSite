7bbbcd73d1b28be89b92a9eec6eb112b4e842ada
Takashi Wickes <twickes@ideo.com>
2019-02-11 14:09:18 -0800

    Updates

---
+++ precache-manifest.2a67ee43de20cfd6106bf1fdcc87f711.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9251d4ba2b4d58ae69683f887c52328c",
+    "url": "/Playshop-LandingSite/static/media/InspirationTourBalloon.9251d4ba.png"
+  },
+  {
+    "revision": "f53d5540b9985f5b23ba",
+    "url": "/Playshop-LandingSite/static/css/main.b693c1c4.chunk.css"
+  },
+  {
+    "revision": "767877f033b6c343f52c6d13c9bc9376",
+    "url": "/Playshop-LandingSite/static/media/PopUpLine.767877f0.svg"
+  },
+  {
+    "revision": "e0d7ad3207f0df155da3",
+    "url": "/Playshop-LandingSite/static/js/1.e0d7ad32.chunk.js"
+  },
+  {
+    "revision": "8e88e592e7d5b972a75c",
+    "url": "/Playshop-LandingSite/static/js/runtime~main.8e88e592.js"
+  },
+  {
+    "revision": "7bafe64bceb97aa412295b291671dd57",
+    "url": "/Playshop-LandingSite/static/media/D4Play_logo.7bafe64b.jpg"
+  },
+  {
+    "revision": "f53d5540b9985f5b23ba",
+    "url": "/Playshop-LandingSite/static/js/main.f53d5540.chunk.js"
+  },
+  {
+    "revision": "f4d83905ffbf3a34e8dc59a834e12405",
+    "url": "/Playshop-LandingSite/static/media/moon.f4d83905.png"
+  },
+  {
+    "revision": "204139cb17d3eb0ef978df36a6b50b93",
+    "url": "/Playshop-LandingSite/static/media/SummaryLine.204139cb.svg"
+  },
+  {
+    "revision": "b7bd448a99fdb3d4949abcf96804cfd5",
+    "url": "/Playshop-LandingSite/static/media/InspirationLine.b7bd448a.svg"
+  },
+  {
+    "revision": "718aa2237cf9d12580feddb0f8c52b9f",
+    "url": "/Playshop-LandingSite/static/media/WorkshopLine.718aa223.svg"
+  },
+  {
+    "revision": "e0d7ad3207f0df155da3",
+    "url": "/Playshop-LandingSite/static/css/1.fc5bf211.chunk.css"
+  },
+  {
+    "revision": "b90176ba0d4f7b565a24cde5755dc0df",
+    "url": "/Playshop-LandingSite/index.html"
+  }
+];
