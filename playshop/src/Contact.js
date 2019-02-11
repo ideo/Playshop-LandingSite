a3c310e4d9425f9d22b69b2f1eaa9071203d8515
Takashi Wickes <twickes@ideo.com>
2019-02-11 12:53:39 -0800

    Added ability to send form to Google Sheets
    Close #7

--- playshop/src/Contact.js
+++ playshop/src/Contact.js
@@ -7,10 +7,47 @@ import './Contact.css'
 class Contact extends Component {
     constructor(props) {
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
       }
       componentDidMount() {
         // window.addEventListener('scroll', this.handleScroll.bind(this));
       }
+      handleSubmit(event){
+        var data = new FormData(event.target);
+        // console.log(data.keys);
+        var newData = {
+            NAME: data.get("NAME"),
+            COMPANY: data.get("COMPANY"),
+            EMAIL: data.get("EMAIL"),
+            SIZE_OF_GROUP: data.get("SIZE_OF_GROUP"),
+            APPR_DATE: data.get("APPR_DATE"),
+            LOCATION: data.get("LOCATION"),
+            OTHER_COMMENTS: data.get("OTHER_COMMENTS")         }
+
+        var dataString = 
+            "NAME=" + data.get("NAME") +
+            "&COMPANY=" + data.get("COMPANY") +
+            "&EMAIL=" + data.get("EMAIL") +
+            "&SIZE_OF_GROUP=" + data.get("SIZE_OF_GROUP") +
+            "&APPR_DATE=" + data.get("APPR_DATE") +
+            "&LOCATION=" + data.get("LOCATION") +
+            "&OTHER_COMMENTS=" + data.get("OTHER_COMMENTS");
+
+        var json = JSON.stringify(newData);
+        console.log(data);
+        console.log(newData);
+        console.log(json);
+        var url = 'https://script.google.com/macros/s/AKfycbyaTfz5ip_za3J958BO2w0X5jJ8EzPShvotuayJ67iEgGxjnA4/exec?' + dataString;
+        event.preventDefault();
+        fetch(url, {
+            method: "GET",
+            dataType: "json",
+            data: newData
+        }).then(
+            console.log("Succed!")
+        )
+      }
+
       render() {
         return (
           <div className="Contact">
@@ -61,15 +98,15 @@ class Contact extends Component {
                                 & we hope we’ll leave your team inspired after.</p><br />
                         </div>
                         <div className="form">
-                            <form>
-                                <input type="text" name="name" placeholder="Name"></input> <br/>
-                                <input type="text" name="company" placeholder="Company"></input> <br/>
-                                <input type="text" name="email" placeholder="Email"></input> <br/>
-                                <input type="text" name="sizeGroup" placeholder="Size of Group"></input> <br/>
-                                <input type="text" name="apprDate" placeholder="Approximate Date"></input> <br/>
-                                <input type="text" name="location" placeholder="Location"></input> <br/>
-                                <input type="text" name="location" placeholder="anything else? questions, comments? We're all ears."></input> <br/>
-                                <input className="submitButton" type="submit" value="Submit" />
+                            <form onSubmit={this.handleSubmit}>
+                                <input type="text" name="NAME" placeholder="Name"></input> <br/>
+                                <input type="text" name="COMPANY" placeholder="Company"></input> <br/>
+                                <input type="text" name="EMAIL" placeholder="Email"></input> <br/>
+                                <input type="text" name="SIZE_OF_GROUP" placeholder="Size of Group"></input> <br/>
+                                <input type="text" name="APPR_DATE" placeholder="Approximate Date"></input> <br/>
+                                <input type="text" name="LOCATION" placeholder="Location"></input> <br/>
+                                <input type="text" name="OTHER_COMMENTS" placeholder="anything else? questions, comments? We're all ears."></input> <br/>
+                                <input className="submitButton" type="submit" id="submit-form" value="Submit"/>
                             </form>
                         </div>
                 </div>
