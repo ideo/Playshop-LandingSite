c65b3184620af29cf63ab719d736ebe41c2451f2
Takashi Wickes <twickes@ideo.com>
2019-02-14 10:13:16 -0800

    Added all the edits
    Close #16

--- playshop/src/Home.js
+++ playshop/src/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './assets/D4Play_logo.jpg';
 import inspirationTourBalloon from './assets/InspirationTourBalloon.png';
 import handsOfInspiration from './assets/handsOfInspiration.png';
-import telescope from './assets/tele@3x.png';
+import telescope from './LightbulbAnimation/LightBulb-Strip.png';
 import { Link } from 'react-router-dom'
 import { ReactComponent as SummaryLine } from './assets/SummaryLine.svg';
 import { ReactComponent as InspirationLine } from './assets/InspirationLine.svg';
 import { ReactComponent as WorkshopLine } from './assets/WorkshopLine.svg';
 import { ReactComponent as PopUpLine } from './assets/PopUpLine.svg';
 import playLab from './assets/toy-lab-brainstorm.jpg'
+import postIts from './assets/post-its.png'
 import './App.css';
 import WorkshopCard from './WorkshopCard.js';
 import Contact from './Contact.js';
@@ -26,6 +27,17 @@ class Home extends Component {
   }
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+  lightbulbClicked() {
+    console.log("CLICKED!!");
+    this.setState({bulbAnimation: {animation: "none"}})
+    setTimeout(() => {
+      this.setState({bulbAnimation: {animation: "spriteSheet-6frame 0.5s steps(6)"}})
+    }, 10);
+    // setTimeout(() => {
+    //   this.setState({bulbAnimation: {}})
+    // }, 1000);
+    // this.setState({bulbAnimation: {animation: "animation: none"}})
   }
   render() {
     return (
@@ -47,8 +59,8 @@ class Home extends Component {
             </div>
             <div className="heroTextContainer col6">
               <div className="heroText">
-                <h1>Make with Us in Playshops</h1>
-                <p>Generate new concepts, build innovative culture, and learn new design tools with IDEO's Play Lab</p>
+                <h1>From Play <br/>to Innovation Workshops</h1>
+                <p>Generate new concepts, <br/>build innovative culture, <br/>and enjoy the process <br/>with IDEO's Play Lab</p>
                 <Link to={"/contact"}>
                     <button>Join Us</button>
                 </Link>
@@ -61,9 +73,12 @@ class Home extends Component {
                 <SummaryLine className="backPath" id="clipper"/>
               </div>
               <div className="summary">
-                  <h2>here's the deal</h2>
-                  <h1>IDEO's Play Lab offers a design-thinking workshop with a playful twist!</h1>
-                  <p>Your team will walk away with new tools, new ideas, and new methodologies for innovation. Perfect for tackling old challenges, building positive company culture, and breaking down organizational barriers.</p>
+              <h1>A design-thinking workshop with a playful twist!</h1>
+
+                  <p>Building upon a rich history as part of a world-renowned design firm, IDEO Play Lab will inspire your team to unleash their creativity & innovation.</p>
+              </div>
+              <div className="postIts col6">
+                <img src={postIts}></img>
               </div>
             </div>
             <div className="workshopCardsContainer">
@@ -71,8 +86,10 @@ class Home extends Component {
                 <InspirationLine />
                 <InspirationLine className="backPath"/>
               </div>
-              <img src={telescope} className="telescope" />
-              <h1>We will create a conducive environment for collaborative ideation through playful methods that encourage creative confidence and thinking out of the box, in order to push ideas beyond what has previously been imagined.</h1>
+              <div className="lightbulbAnimation" onClick={this.lightbulbClicked.bind(this)}>
+                <img src={telescope} className="telescope" style={this.state.bulbAnimation}/>
+              </div>
+              <h1>Our workshops create a conducive environment for collaborative ideation through playful methods. Are you ready to build creative confidence and push your ideas beyond what has previously been imagined?</h1>
 
               <div className="qualityCardsContainer">
               <h2>In the course of this interactive experience, participants will:</h2>
@@ -84,31 +101,31 @@ class Home extends Component {
                 <div className="qualityCard">
                   <img src={handsOfInspiration} />
                   <h3>1. Get Inspired</h3>
-                  <p>Be introduced to analogous inspiration that exposes them to experiences that go beyond your industry to help jump-start fresh ideas</p>
+                  <p>Be introduced to compelling experiences that that will jump start fresh ideas.</p>
                 </div>
 
                 <div className="qualityCard">
                   <img src={handsOfInspiration} />
                   <h3>2. Set Focus</h3>
-                  <p>Identify rich opportunity areas to brainstorm.</p>
+                  <p>Identify rich opportunity areas that your team is well-positioned to purse.</p>
                 </div>
 
                 <div className="qualityCard">
                   <img src={handsOfInspiration} />
-                  <h3>3. Gather Tools</h3>
-                  <p>Master the rules of successful brainstorming and participate in activities to boost their  Creative Confidence</p>
+                  <h3>3. Learn New Tools</h3>
+                  <p>Activiely engage in activities that introduce Design Thinking & boost Creative Confidence.</p>
                 </div>
                 <div className="spacer"></div>
                 <div className="qualityCard">
                   <img src={handsOfInspiration} />
-                  <h3>4. Create</h3>
-                  <p>Brainstorm new concepts</p>
+                  <h3>4. Generate Ideas</h3>
+                  <p>Master the rules of successful brainstorming & improve your quality & quantity of ideas.</p>
                 </div>
 
                 <div className="qualityCard">
                   <img src={handsOfInspiration} />
-                  <h3>5. Share</h3>
-                  <p>Prototype and share back these concepts at varying levels of fidelity</p>
+                  <h3>5. Make Concepts Tangible</h3>
+                  <p>Bring ideas to life and understand the value of prototyping throughout the design process.</p>
                 </div>
 
               </div>
@@ -124,10 +141,10 @@ class Home extends Component {
 
               <div className="emailContainer" ref={this.emailContainerRef}>
                 <div className="background" style={{height: this.state.emailContainerHeight}}></div>
-                <h1>We also do other sessions, such as inspiration tours, pop-ups, projects, & more.
+                <h1>Looking for more? <br/>We also lead inspiration tours, <br/>welcome speaking engagements <br/>& run full design sprints and projects.
                   <br/>
                   <br/>
-                  Reach out to us at:<br/>
+                  Please reach out to us at:<br/>
                   <a>play@ideo.com</a>
                 </h1>
               </div>
